573a78f9f2496d7ef6ab4c4500c86272d69dd1bf
Guillaume Conchon <guillaume.conchon@gmail.com>
2015-03-19 06:42:02 +0100

    Adding twitter bootstap

--- src/main/java/net/emcheris/spboot/Application.java
+++ src/main/java/net/emcheris/spboot/Application.java
@@ -10,4 +10,5 @@ public class Application {
         app.setShowBanner(false);
         app.run(args);
     }
+    
 }
--- src/main/java/net/emcheris/spboot/web/controller/SampleController.java
+++ src/main/java/net/emcheris/spboot/web/controller/SampleController.java
@@ -2,7 +2,9 @@ package net.emcheris.spboot.web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +18,10 @@ public class SampleController {
     String home() {
         return "Mon premier projet Spring boot : " + name;
     }
-
+    
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="Guillaume") String name, Model model) {
+    	model.addAttribute("name", name);
+    	return "index/hello";
+    }
 }
